db1e5ef18c08a79fac2c40a33e3b4efed573bdc9
VicMux <vmuralid@yahoo.com>
2025-02-25 22:01:51 -0500

    Flask testing

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, make_response, request
+from EmotionDetection import emotion_detection as ed 
+
+app = Flask(__name__)
+
+@app.route("/emotionDetector")
+def emotionDetector():
+    statement = request.args.get('q')
+    if not statement == request.args.get('q'):
+        return {"message" : "Query parameter 'q' is missing"}, 422
+
+    resp = ed.emotion_detector(statement)
+    dominant_emotion = resp['dominant_emotion']
+
+    sentiment = "For the given statement, the system response is " + resp + " The dominant emotion is " + dominant_emotion
+    return {"message" : sentiment}, 200
